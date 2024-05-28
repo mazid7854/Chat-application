7f08bbbced911ae329c883b29dcbe8ec39e69b4f
mazid7854 <mazidali218@gmail.com>
2024-05-28 13:34:04 +0530

    message and conversation model created

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,36 +1,39 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-  },
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
 
-  gender: {
-    type: String,
-    required: true,
-    enum: ["male", "female"],
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
   },
-  profilePicture: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
