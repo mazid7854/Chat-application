4ec31b833c0602a6fa7ba92e3ca7892b61b2bb38
mazid ali <mazidali218@gmail.com>
2025-02-27 13:59:26 +0530

    add login page and background image; update App component and styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Login from "./pages/login/Login";
 
-const App = () => {
-  return <></>;
-};
+function App() {
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Login />
+    </div>
+  );
+}
 
 export default App;
