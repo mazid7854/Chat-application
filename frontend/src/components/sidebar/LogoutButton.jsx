e593e7af236aeb685e452611afa07f97920e1f81
mazid ali <mazidali218@gmail.com>
2025-02-28 03:49:25 +0530

    implement logout functionality with API call; handle success and error notifications

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -2,12 +2,20 @@ import { BiLogOut } from "react-icons/bi";
 import { useDispatch } from "react-redux";
 import { logout } from "../../redux/userSlice.js";
 import toast from "react-hot-toast";
+import axios from "axios";
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
   const handleLogout = () => {
-    dispatch(logout());
-    toast.success("Logged out successfully!");
+    axios
+      .post("api/auth/logout")
+      .then(() => {
+        dispatch(logout());
+        toast.success("Logged out successfully!");
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message || "Logout failed");
+      });
   };
   return (
     <div className="mt-auto">
