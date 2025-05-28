16588e322b41199998f06c649ce15d4e9012730c
mazid ali <mazidali218@gmail.com>
2025-05-28 11:15:33 +0530

    chore: remove homepage field from package.json

--- frontend/src/socket.io/socket.js
+++ frontend/src/socket.io/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://chat-application-uilo.onrender.com", {
+export const socket = io("http://127.0.0.1:5000", {
   autoConnect: false, // Prevent auto-connection, we'll handle it manually
   withCredentials: true, // Allow cookies for authentication if needed
   transports: ["websocket"], // Force WebSocket (improves stability)
